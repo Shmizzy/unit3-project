aa9de326279f868ab532826a95a313edff70954f
izzylppz <https://github.com/Shmizzy>
2024-05-22 18:13:33 -0500

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar';
+import { Routes,Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Signup from './components/Signup/Signup.jsx';
+import Login from './components/Login/Login.jsx';
+
 
 const App = () => {
+
   return(
     <main>
-      <Navbar />
-      <h1>WELCOME</h1>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </main>
-  )
+     );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import './navBar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
     return(
         <div className="navBar">
            <h3 className='battle'>Battle</h3>
            <div className='navButtons'>
-            <button>Sign In</button>
-            <button>Register</button>
+            <Link to='/login'><button>Sign In</button></Link>
+            <Link to='/register'><button>Register</button></Link>
            </div>
         </div>
     )
