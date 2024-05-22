1270161db88741cd87686d9a73cbcf3e0a2384be
chris <jimenezchristopher5@gmail.com>
2024-05-22 18:22:41 -0500

    fixed bug where add and remove of players to and from MyTeam was based on player name and not id

--- src/components/MyTeam/MyTeam.jsx
+++ src/components/MyTeam/MyTeam.jsx
@@ -6,10 +6,10 @@ const MyTeam = (props) => {
     const {myTeam, setMyTeam} = props;
 
     //Functions
-    const handleRemove = (player) => {
+    const handleRemove = (subIndex) => {
         console.log(myTeam)
-        const filteredTeam = [...myTeam].filter((mainPlayer) => {
-            return mainPlayer !== player;
+        const filteredTeam = [...myTeam].filter((mainPlayer, mainIndex) => {
+            return mainIndex !== subIndex;
         });
         setMyTeam(filteredTeam);
     };
@@ -19,12 +19,12 @@ const MyTeam = (props) => {
     <div className="myTeamContainer">
         <h2>My Team: </h2>
         <ul>
-            {myTeam.map((player)=>{
+            {myTeam.map((player, index)=>{
                 return (
-                    <li key={player._id}>
+                    <li key={index}>
                         <h3>Player: {player.first_name} {player.last_name}</h3>
                         <h3>Team: {player.team}</h3>
-                        <button onClick={()=> handleRemove(player)}>Remove</button>
+                        <button onClick={()=> handleRemove(index)}>Remove</button>
                     </li>
                 )
             })}
