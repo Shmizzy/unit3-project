e2e1628b0122aeb1d685ac32181be00af0b89505
izzylppz <https://github.com/Shmizzy>
2024-05-23 11:58:10 -0500

    navbarAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-
-import './App.css'
+import { createContext,useState } from 'react';
 import { Routes,Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard.jsx';
 import Signup from './components/Signup/Signup.jsx';
 import Login from './components/Login/Login.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import * as authService from "./services/userAuth.js";
 
+export const AuthedUserContext = createContext(null);
 
 
 const App = () => {
+  const [user, setUser] = useState(authService.getUser());  
 
+  const handleSignout = () => {
+    authService.signOut();
+    setUser(null);
+  }
 
   return(
-    <main>
+    <>
+      <Navbar handleSignout={handleSignout} user={user}/>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/register' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Dashboard handleSignout={handleSignout}/>} />
+        <Route path='/register' element={<Signup setUser={setUser}/>} />
+        <Route path='/login' element={<Login setUser={setUser}/>} />
       </Routes>
-    </main>
+    </>
      );
 }
 
