fdcea0f90f6a5f80a5c95a225856726b47ad6d20
chris <jimenezchristopher5@gmail.com>
2024-05-22 19:26:29 -0500

    merged main

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 
 import './App.css'
-<<<<<<< HEAD
 import { Routes,Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard.jsx';
 import Signup from './components/Signup/Signup.jsx';
 import Login from './components/Login/Login.jsx';
-=======
-
->>>>>>> e7cc918e3edeb2d79542a375526fa592b843f65f
 
 
 
 const App = () => {
 
 
-<<<<<<< HEAD
   return(
     <main>
       <Routes>
@@ -25,26 +20,6 @@ const App = () => {
     </main>
      );
 }
-=======
-  useEffect(()=>{
-    fetchPlayersDatabase();
-  }, [])
 
-
-
-  return (
-
-    <>
-      
-      <PlayerList playerList={playerList} setMyTeam={setMyTeam} myTeam={myTeam} />
-
-      <MyTeam myTeam={myTeam} setMyTeam={setMyTeam}/>
-
-    </>
-
-  )
->>>>>>> e7cc918e3edeb2d79542a375526fa592b843f65f
-
-}
 
 export default App;
